2d8bbb902d5acb64d47c48728071c4b95f522a7d
Scott Davis <scott.sheri@gmail.com>
2014-08-20 23:09:26 -0600

    url variables and comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// this will save us some typing later on
+var baseUrl = 'http://mapserv.utah.gov/ArcGIS/rest/services/';
+
+// the four image services that we are working with
+var urls = {
+    hro2012: baseUrl + 'AerialPhotography_Color/HRO2012Color6Inch_4Band/ImageServer',
+    hro2009: baseUrl + 'AerialPhotography_Color/HRO2009_Color1Foot/ImageServer',
+    uao2003: baseUrl + 'AerialPhotography_Color/UAO2003_Color1Foot/ImageServer',
+    doq1990: baseUrl + 'AerialPhotography_BlackWhite/DOQ1990s_1Meter/ImageServer'
+};
+
 console.log('hello');
 
 function init() {
